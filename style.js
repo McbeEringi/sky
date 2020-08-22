8554251a7c7b7dc769e9130fbb9ef78bf34cb7b1
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2020-08-22 13:20:07 +0900

    -

--- style.js
+++ style.js
@@ -1,7 +1,6 @@
 document.write('<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet" />');
 document.write(`<style>
-:root{background:#222;color:#fff;text-shadow:0 0 4px #222;word-wrap:break-word;}
-*{font-family:"M PLUS Rounded 1c",sans-serif;}
+:root{background:#222;font-family:"M PLUS Rounded 1c",sans-serif;color:#fff;text-shadow:0 0 4px #222;word-wrap:break-word;}
 #bg{position:fixed;top:0;left:0;z-index:-16;width:100vw;height:100vh;}
 a:link,a:visited{color:#aef;}a:hover{color:#8af;}a:active{color:#48f;}
 .flex{display:flex;justify-content:space-evenly;flex-wrap:wrap;align-items:flex-start;}
