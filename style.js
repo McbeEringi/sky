512f22eec2c3476d1cb700fca727944ac2732647
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2020-08-31 19:49:47 +0900

    -

--- style.js
+++ style.js
@@ -1,7 +1,7 @@
 document.write('<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet" />');
 document.write(`<style>
 :root{background:#222;font-family:"M PLUS Rounded 1c",sans-serif;color:#fff;text-shadow:0 0 4px #222;word-wrap:break-word;}
-#bg{position:fixed;top:0;left:0;z-index:-16;width:100vw;height:100vh;}
+#bg{position:fixed;top:0;left:0;z-index:-16;width:100vw;height:100vh;transition:1s;}
 a:link,a:visited{color:#aef;}a:hover{color:#8af;}a:active{color:#48f;}
 .flex{display:flex;justify-content:space-evenly;flex-wrap:wrap;align-items:flex-start;}
 </style>`);
@@ -13,5 +13,6 @@ const bgcol = [
 	"#cce5f0,#ced980",//day
 	"#f08300,#f8b862",//dusk
 	"#192f60,#274a78"//night
-];
-bg.style.background = "linear-gradient("+bgcol[[3,0,1,1,2,3][Math.floor(Math.max(new Date().getHours()-1,0)*.25)]]+")";
+],bgset=()=>bg.style.background=`linear-gradient(${bgcol[[3,0,1,1,2,3][Math.floor(Math.max(new Date().getHours()-1,0)*.25)]]})`;
+bgset();
+document.addEventListener('visibilitychange',()=>if(document.visibilityState=='visible')bgset())
