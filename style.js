badcfc0f5cc57c392a14e324b1e179a876c8a033
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2021-02-02 17:40:57 +0900

    Update style.js

--- style.js
+++ style.js
@@ -19,10 +19,9 @@ a:link,a:visited{color:#aef;}a:hover{color:#8af;}a:active{color:#48f;}
 var urlq={};location.search.substr(1).split('&').map(x=>x.split('=')).map(x=>urlq[x[0]]=x[1]);console.log(urlq);
 const e_bg=document.createElement('div');e_bg.setAttribute('id','bg');document.body.appendChild(e_bg);
 const e_img=document.createElement('img');e_img.setAttribute('src','https://mcbeeringi.github.io/sky/img/sky.svg');e_img.setAttribute('alt','background');e_bg.appendChild(e_img);
-document.addEventListener('readystatechange',e=>{if(e.target.readyState=='interactive'){
-	const e_bb=document.createElement('div');e_bb.setAttribute('id','hisbackb');e_bb.onclick=()=>history.back();document.body.appendChild(e_bb);
-	if(urlq.pwa=='1')hisbackb.style.display='block';
-}},false);
+const e_bb=document.createElement('div');e_bb.setAttribute('id','hisbackb');e_bb.onclick=()=>history.back();document.body.appendChild(e_bb);
+if(urlq.pwa=='1')hisbackb.setAttribute('style','display:block;');
+//document.addEventListener('readystatechange',e=>{if(e.target.readyState=='interactive'){}},false);
 const bgcol=[
 	"#fff1cf,#ced980",//morn
 	"#cce5f0,#ced980",//day
