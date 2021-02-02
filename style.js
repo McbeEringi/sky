9f88caa2a526367d2322ad6425bdd2e404727806
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2021-02-02 11:52:33 +0900

    -

--- style.js
+++ style.js
@@ -1,3 +1,12 @@
+const idbName='sky_idb',idbVer=1;
+let idb=indexedDB.open(idbName,idbVer);
+idb.onupgradeneeded=e=>{
+	console.log('idb upgrade');
+	idb.result.createObjectStore('stuff');
+}
+idb.onsuccess=e=>{console.log('idb open success');if(!['0',undefined].includes(localStorage.sky_bgmode))bgset();};
+idb.onerror=e=>{console.log('idb open error: '+idb.errorCode);localStorage.sky_bgmode='0';bgset();};
+
 document.body.insertAdjacentHTML('afterbegin',`<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet" />
 <style>
 :root,.style{background:#222;font-family:"M PLUS Rounded 1c",sans-serif;color:#fff;text-shadow:0 0 4px #222;word-wrap:break-word;}
@@ -9,8 +18,8 @@ a:link,a:visited{color:#aef;}a:hover{color:#8af;}a:active{color:#48f;}
 </style>`);
 var urlq={};location.search.substr(1).split('&').map(x=>x.split('=')).map(x=>urlq[x[0]]=x[1]);console.log(urlq);
 const e_bg=document.createElement('div');e_bg.setAttribute('id','bg');document.body.appendChild(e_bg);
+const e_img=document.createElement('img');e_img.setAttribute('src','https://mcbeeringi.github.io/sky/img/sky.svg');e_img.setAttribute('alt','background');e_bg.appendChild(e_img);
 document.addEventListener('readystatechange',e=>{if(e.target.readyState=='interactive'){
-	const e_img=document.createElement('img');e_img.setAttribute('src','https://mcbeeringi.github.io/sky/img/sky.svg');e_img.setAttribute('alt','background');e_bg.appendChild(e_img);
 	const e_bb=document.createElement('div');e_bb.setAttribute('id','hisbackb');e_bb.onclick=()=>history.back();document.body.appendChild(e_bb);
 	if(urlq.pwa=='1')hisbackb.style.display='block';
 }},false);
@@ -19,12 +28,21 @@ const bgcol=[
 	"#cce5f0,#ced980",//day
 	"#f08300,#f8b862",//dusk
 	"#192f60,#274a78"//night
-],bgset=(x,b)=>{
+];
+const bgset=(x,b)=>{
+	let bgimg;
 	switch(b||localStorage.sky_bgmode){
-		case'1':bg.style.backgroundImage=`url(${localStorage.sky_bgimg})`;break;
-		case'2':bg.style.backgroundImage=localStorage.sky_bgcode.replace(/IMAGE/g,`url(${localStorage.sky_bgimg})`);break;
-		default:bg.style.backgroundImage=`linear-gradient(${bgcol[x!=undefined?x:([3,0,1,1,2,3][Math.floor(Math.max(new Date().getHours()-1,0)*.25)])]})`;
+		case'1':
+			e_img.setAttribute('style','display:none;');
+			bgimg=idb.result.transaction('stuff','readwrite').objectStore('stuff').get('bgimg');
+			bgimg.onsuccess=e=>bg.style.backgroundImage=`url(${URL.createObjectURL(e.target.result)})`;
+			break;
+		case'2':e_img.setAttribute('style','display:none;');bg.style.backgroundImage=localStorage.sky_bgcode;break;
+		default:e_img.setAttribute('style','');bg.style.backgroundImage=`linear-gradient(${bgcol[x!=undefined?x:([3,0,1,1,2,3][Math.floor(Math.max(new Date().getHours()-1,0)*.25)])]})`;
 	}
 };
-bgset();
-document.addEventListener('visibilitychange',()=>{if(document.visibilityState=='visible')bgset();})
+{
+	const bgset_=()=>{if(['0',undefined].includes(localStorage.sky_bgmode))bgset();};
+	bgset_();
+	setTimeout(()=>{bgset_();setInterval(bgset_,3600000)},new Date().getTime()%3600000);
+}
