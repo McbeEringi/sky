2c2649e6abf9b57ff0d241ec7d424d2a9044ac2b
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2021-03-31 22:34:42 +0900

    -

--- seq.js
+++ seq.js
@@ -271,7 +271,7 @@ load=()=>{
 sopt={
 	group:{
 		name:"group_",
-		pull:(to,from)=>to.el.id=='pallet'?'clone':true,
+		pull:(to,from)=>to.el.id=='palette'?'clone':true,
 		put:true,
 	},
 	onStart:()=>dispCur.style.opacity='0',onEnd:()=>dispCur.style.opacity='1',
@@ -541,7 +541,7 @@ new Sortable(tpl,{
 		if(e.pullMode=='clone'&&e.item.children.length)Array.from(e.item.children,e=>new Sortable(e,sopt));
 	},
 });
-new Sortable(pallet,{
+new Sortable(palette,{
 	group:{
 		name:'group_',
 		pull:(to,from)=>to.el.id=='trash'?true:'clone',
