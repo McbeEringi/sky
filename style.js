313ffe213b5cf2aa7fd3b7cf8e05ee843f318072
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2021-05-31 04:22:31 +0900

    -

--- style.js
+++ style.js
@@ -17,7 +17,7 @@ document.body.insertAdjacentHTML('afterbegin',`<link rel="stylesheet" href="http
 a:link,a:visited{color:#aef;}a:hover{color:#8af;}a:active{color:#48f;}
 #hisbackb{display:none;position:fixed;left:2px;bottom:2px;width:48px;height:48px;border-radius:12px;background:#2228 url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' style='filter:drop-shadow(0 0 4px %23f00);'%3E%3Cpath d='M32,8L16,24L32,40' stroke='%23fea' stroke-width='2px' fill='%230000'/%3E%3C/svg%3E");}
 </style>`);
-var urlq={};location.search.substr(1).split('&').map(x=>x.split('=')).map(x=>urlq[x[0]]=x[1]);console.log(urlq);
+var urlq={};location.search.substr(1).split('&').map(x=>x.split('=')).forEach(x=>urlq[x[0]]=x[1]);console.log(urlq);
 const e_bg=document.createElement('div');e_bg.setAttribute('id','bg');document.body.appendChild(e_bg);
 const e_img=document.createElement('img');e_img.setAttribute('src','https://mcbeeringi.github.io/sky/img/sky_.svg');e_img.setAttribute('style','display:none;');e_img.setAttribute('alt','background');e_img.setAttribute('width','1');e_img.setAttribute('height','1');e_bg.appendChild(e_img);
 const e_bb=document.createElement('div');e_bb.setAttribute('id','hisbackb');e_bb.onclick=()=>history.back();document.body.appendChild(e_bb);
@@ -42,3 +42,4 @@ const bgset=(x,b)=>{
 	bgset_();
 	setTimeout(()=>{bgset_();setInterval(bgset_,3600000);},3600000-(new Date().getTime()%3600000));
 }
+window.dispatchEvent(new Event('styexe'));
