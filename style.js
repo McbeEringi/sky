bd80e275ceacb292142dd8d72ba46fc54a303a62
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2021-07-07 21:10:50 +0900

    -

--- style.js
+++ style.js
@@ -34,7 +34,7 @@ idb.onerror=e=>{console.log('idb open error',e);bgset(-1,'0');};
 document.body.insertAdjacentHTML('afterbegin',`<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=M+PLUS+Rounded+1c&display=swap" media="print" onload="this.media='all'">
 <style>
 :root,.style{background:#222;font-family:"M PLUS Rounded 1c",sans-serif;color:#fff;text-shadow:0 0 4px #222;word-wrap:break-word;}*{-webkit-tap-highlight-color:#0000;}
-#bg{position:fixed;top:0;left:0;z-index:-16;width:100vw;height:100vh;transition:background 1s;pointer-events:none;background-size:cover;background-position:center;user-select:none;-webkit-user-select:none;}
+#bg{position:fixed;top:0;left:0;z-index:-16;width:100vw;height:100vh;transition:background 1s;pointer-events:none;background:center/cover;user-select:none;-webkit-user-select:none;}
 #bg>img{opacity:.2;width:100vmin;height:auto;float:right;transform:translateX(25%);background:none;}
 a:link,a:visited{color:#aef;}a:hover{color:#8af;}a:active{color:#48f;}
 #hisbackb{display:none;position:fixed;left:2px;bottom:2px;width:48px;height:48px;border-radius:12px;background:#2228 url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' style='filter:drop-shadow(0 0 4px %23f00);'%3E%3Cpath d='M32,8L16,24L32,40' stroke='%23fea' stroke-width='2px' fill='%230000'/%3E%3C/svg%3E");}
