dceab5826e27a4437d87e678830c93164eb93500
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2021-09-06 03:14:36 +0900

    -

--- skysw.js
+++ skysw.js
@@ -1,6 +1,6 @@
 //https://developer.mozilla.org/ja/docs/Web/Progressive_web_apps/Offline_Service_workers
 //https://developers.google.com/web/fundamentals/primers/service-workers?hl=ja
-const cacheName='cache210905_1',STATIC_DATA=[
+const cacheName='cache210906_0',STATIC_DATA=[
 	'style.js',
 	'img/sky.svg','img/sky_.svg','img/sky.png','img/sky_192.png',
 	'img/tex.webp',
