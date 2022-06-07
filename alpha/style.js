6d6b6985403e312e047ca904bd00d324ff629d66
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2022-06-07 20:50:03 +0900

    Add files via upload

---
+++ alpha/style.js
@@ -0,0 +1,12 @@
+document.body.insertAdjacentHTML('afterbegin',`<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=M+PLUS+Rounded+1c&display=swap" media="print" onload="this.media='all'"><style>
+:root{
+	--bc:#222;--fc:#fff;--l:#fea;
+	--b0:#48f;--b1:#8af;--b2:#aef;
+	--p0:#84f;--p1:#a8f;--p2:#eaf;
+	background-color:var(--bc);
+}
+*{-webkit-tap-highlight-color:#0000;}
+#bg{position:fixed;width:100%;height:100%;z-index:-1;background-size:cover;}
+:root{color:var(--fc);font-family:"M PLUS Rounded 1c",sans-serif;}
+:link{color:var(--b2);}:link:hover{color:var(--b1);}:link:active{color:var(--b0);}:visited{color:var(--p2);}:visited:hover{color:var(--p1);}:visited:active{color:var(--p0);}
+`);
