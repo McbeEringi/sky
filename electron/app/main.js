5a49602b7cb71d241f596ceda8fa3659dbec017e
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2022-06-18 00:37:01 +0900

    -

---
+++ electron/app/main.js
@@ -0,0 +1,38 @@
+const{
+	app,
+	Menu,
+	BrowserWindow
+}=require('electron'),
+init=()=>{
+	Menu.setApplicationMenu(Menu.buildFromTemplate([
+		{role:'appMenu'},
+		{role:'fileMenu'},
+		{role:'editMenu'},
+		{role:'viewMenu'},
+		{role:'windowMenu'},
+		{
+			role:'help',
+			submenu:[
+				{
+					label:'website',
+					click:()=>{
+						const win=new BrowserWindow({show:false});
+						win.once('ready-to-show',()=>win.show());
+						win.loadURL('https://mcbeeringi.github.io/');
+					}
+				},
+				{label:'dupe',click:init}
+			]
+		}
+	]));
+	const win=new BrowserWindow({
+		backgroundColor:'#214',
+		titleBarStyle:'hidden',autoHideMenuBar:true,show:false,//kiosk:true,
+		webPreferences:{devTools:false}
+	});
+	win.once('ready-to-show',()=>win.show());
+	win.loadURL(`file://${__dirname}/index.html`);
+};
+
+app.on('ready',init);
+app.on('window-all-closed',app.quit);
